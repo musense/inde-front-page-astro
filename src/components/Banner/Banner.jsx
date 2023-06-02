8009573a669a33b3b80ff7ac1d554845eb1cbf66
musense <musense.ad@gmail.com>
2023-06-02 13:13:16 +0800

    分類頁banner取消外鍊

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -26,9 +26,9 @@ const Banner = React.forwardRef(function Banner(
 
     // console.log(`🚀 ~ file tagPage.jsx:122 ~ banner ~ imageMap.get('${category}'):`, imageMap.get(category))
     return image && (<div ref={ref} className={`section ${styles.section}`}>
-        <a href={'https://zoobet168.com/'} target="_blank" rel="noopener noreferrer" >
+        {/* <a href={'https://zoobet168.com/'} target="_blank" rel="noopener noreferrer" > */}
             <img src={image} style={{ width: '100%', objectFit: 'cover', objectPosition: 'center' }} />
-        </a>
+        {/* </a> */}
     </div>)
 }
 );
